7f5fe08d8ea8db4f255efcd155191b29e4d58da7
Nikiliks <nktalpeev@gmail.com>
2024-12-22 16:25:00 +0500

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -14,7 +14,7 @@ async def return_dict() -> dict:
 async def append_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', examples='Example')],
                     age: Annotated[int, Path(le=120, ge=18, description='Enter age', examples='18')])-> str:
     user_id = str(int(max(users, key=int)) + 1)
-    users[user_id] = f"Имя:{username}, возраст:{age}"
+    users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User{user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
@@ -27,7 +27,7 @@ async def update_users(user_id: Annotated[int, Path(gt=0, lt=100, description='E
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: int = Path(gt=1, lt=100, description='Enter User ID', examples='1')) -> str:
     users.pop(str(user_id))
-    return f"User{user_id} has been deleted"
+    return f"User {user_id} has been deleted"
 
 #Запуск: uvicorn module_16_3:app --reload
 
